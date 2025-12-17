e7ed7025ab38761cc62420fa5f6d8d3986cb2cbb
hemadhami001 <hemadhami365@gmail.com>
2025-12-17 21:31:53 +0545

    first commit

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,6 @@ app.get("/api/notes", (req, res) => {
     res.status(200).send("You got 5 notes");
 } );
 
-app.post("/api/hello", (req, res) => {
-    // create a new note
-    res.status(201).send("You note has been created successfully");
-} );
 
 app.listen(5001, () => {
   console.log("Server running on port 5001");

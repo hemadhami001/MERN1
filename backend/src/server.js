4f4354be067c50b72930ce7330647346ffa32f32
hemadhami001 <hemadhami365@gmail.com>
2025-12-20 22:07:17 +0545

    database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
+
+connectDB();
 
 app.use("/api/notes", notesRoutes);
 
@@ -9,3 +12,5 @@ app.use("/api/notes", notesRoutes);
 app.listen(5001, () => {
   console.log("Server running on port 5001");
 });
+
+// mongodb+srv://hemadhami365_db_user:30ZVTcXUGaCsMvXB@cluster0.0xiwgu2.mongodb.net/?appName=Cluster0

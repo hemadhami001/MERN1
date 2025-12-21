0549f44efe1bcdd699680ddd16b5398d77b5e316
hemadhami001 <hemadhami365@gmail.com>
2025-12-21 21:40:47 +0545

    database connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,22 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log(process.env.MONGO_URI);
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
 connectDB();
 
 app.use("/api/notes", notesRoutes);
 
 
-app.listen(5001, () => {
-  console.log("Server running on port 5001");
+app.listen(PORT, () => {
+  console.log("Server running on PORT", PORT);
 });
 
 // mongodb+srv://hemadhami365_db_user:30ZVTcXUGaCsMvXB@cluster0.0xiwgu2.mongodb.net/?appName=Cluster0

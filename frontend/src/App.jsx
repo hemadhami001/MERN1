13ad3dafeb826c7cf61329f18dc9c37e8d3885f5
hemadhami001 <hemadhami365@gmail.com>
2025-12-26 22:17:37 +0545

    react-hot-toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,16 @@ import CreatePage from './pages/CreatePage.jsx'
 import NoteDetailPage from './pages/NoteDetailPage.jsx'
 
 const App = () => {
-  return <div>
+  return (
+  <div>
+    <button onClick={() => toast.success("Congrats!")}>Click me</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/note/:id" element={<NoteDetailPage />} /> 
       </Routes>
     </div>
+  );
 }
 
 export default App;

a5574c14afbdfa64254c736bcc05b184f6a1dc11
hemadhami001 <hemadhami365@gmail.com>
2025-12-28 21:39:14 +0545

    homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import { useState } from 'react';
+import Navbar from '../components/Navbar';
+import RatelimitedUI from '../components/RatelimitedUI';
+import { useEffect } from 'react';
 
 const HomePage = () => {
-  return <div>HomePage</div>
-}
+  const [isRateLimited, setIsRateLimited] = useState(true);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default HomePage
+  useEffect(() =>{
+    const fetchNotes = async () => {
+      try {
+        const res = await fetch("http://localhost:5001/api/notes");
+        const data = await res.json();
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching notes:", error);
+      }
+    }
+    fetchNotes();
+  }, []);
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+
+      {isRateLimited && <RatelimitedUI />}
+    </div>
+  );
+};
+
+export default HomePage;

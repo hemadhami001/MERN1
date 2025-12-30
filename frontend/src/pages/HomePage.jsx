04da0777e2844c95d3188716daea52727bc96ec6
hemadhami001 <hemadhami365@gmail.com>
2025-12-30 22:09:11 +0545

    Cors

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,20 +3,31 @@ import Navbar from '../components/Navbar';
 import RatelimitedUI from '../components/RatelimitedUI';
 import { useEffect } from 'react';
 import axios from 'axios';
+import { toast } from 'react-hot-toast';
+import NoteCard from '../components/NoteCard';
 
 const HomePage = () => {
-  const [isRateLimited, setIsRateLimited] = useState(true);
+  const [isRateLimited, setIsRateLimited] = useState(false);
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() =>{
     const fetchNotes = async () => {
       try {
-        const res = await axios.post("http://localhost:5001/api/notes");
-        
+        const res = await axios.get("http://localhost:5001/api/notes"); 
         console.log(res.data);
+        setNotes(res.data);
+        setIsRateLimited(false);
       } catch (error) {
-        console.error("Error fetching notes:", error);
+        console.error("Error fetching notes:");
+        console.log(error);
+        if(error.response?.status === 429){
+          setIsRateLimited(true);
+        } else {
+          toast.error("Failed to load notes");
+        }
+      } finally {
+        setLoading(false);
       }
     }
     fetchNotes();
@@ -26,6 +37,18 @@ const HomePage = () => {
       <Navbar />
 
       {isRateLimited && <RatelimitedUI />}
+
+       <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} />
+            ))}
+          </div>
+        )}
+       </div>
     </div>
   );
 };
